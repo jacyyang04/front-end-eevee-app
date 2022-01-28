0332cb5c714a2837965964d84ce9e0d9cc9f9d98
jacy <jacyyang04@gmail.com>
2022-01-28 12:15:20 -0800

    remove old key and hardcoded open-charge url

--- src/App.js
+++ src/App.js
@@ -2,21 +2,13 @@ import "./App.css";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 
-const OPEN_CHARGE_API_URL = process.env.OPEN_CHARGE_API;
 const key = process.env.key;
-
-// Params:
-// countrycode = "US"
 
 const getPoiList = () => {
   axios
-    .get(
-      "https://api.openchargemap.io/v3/referencedata/?key=0932b917-00fe-4b0e-b21f-9183fedc8ce2"
-    )
-    //.get(`${OPEN_CHARGE_API_URL}?key=${key}`)
+    .get(`https://api.openchargemap.io/v3/poi?key=${key}`)
     .then((response) => {
       console.log(response);
-      return response.data;
     })
     .catch((error) => {
       console.log(error.response.data);
