bfdcfa4a00acb1160678c992e7df2dacf9e8df6b
jacy <jacyyang04@gmail.com>
2022-01-28 16:16:58 -0800

    add working mapbox

--- README.old.md
+++ README.old.md
@@ -5,6 +5,12 @@ Front End for eevee app Capstone
 Inside terminal in this current repo:
 
 Install axios with: yarn add axios
+Install npm mapbox package: npm install --save react-map-gl
 
 Add env file with: touch .env
-- add open-charge-api key as key
+- add REACT_APP_OPEN_CHARGE=insert opencharge key
+- add REACT_APP_MAPBOX_TOKEN=mapboxtoken
+
+- these variables should contain no spaces and whenever env files are edited, restart react
+
+Install dependencies: yarn install
--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import "./App.css";
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useRef, useEffect, useState } from "react";
+import ReactMapGL from "react-map-gl";
 
-const key = process.env.key;
+// const getPoiList = () => {
+//   axios
+//     .get(`https://api.openchargemap.io/v3/poi?key=${key}`)
+//     .then((response) => {
+//       console.log(response);
+//     })
+//     .catch((error) => {
+//       console.log(error.response.data);
+//     });
+// };
 
-const getPoiList = () => {
-  axios
-    .get(`https://api.openchargemap.io/v3/poi?key=${key}`)
-    .then((response) => {
-      console.log(response);
-    })
-    .catch((error) => {
-      console.log(error.response.data);
-    });
-};
+// const mapboxgl = require("mapbox-gl/dist/mapbox-gl.js");
+
+// mapboxAPIAccessToken = process.env.REACT_APP_MAPBOX;
+
+// const map = new mapboxgl.Map({
+//   container: "map-container",
+//   style: "mapbox://styles/mapbox/streets-v11",
+// });
 
 // &latitude=74.0060&longitude=40.712
+
 function App() {
+  const [viewport, setViewport] = useState({
+    latitude: 47.6062,
+    longitude: 122.3321,
+    zoom: 10,
+    width: "50vw",
+    height: "50vh",
+  });
+
   return (
     <div className="App">
-      <header>Mapbox Branch...</header>
+      <header>eevee App</header>
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+      >
+        SHOW ME YO MAPPPPPP!
+      </ReactMapGL>
     </div>
   );
 }
