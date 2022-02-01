bb1a996fb417695273645fe093f767be87a0c053
jacy <jacyyang04@gmail.com>
2022-01-31 18:30:58 -0800

    add markers code, add pin images

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import ReactMapGL, { Marker } from "react-map-gl";
+// import * as pin from "./images";
 
 // const mapboxgl = require("mapbox-gl/dist/mapbox-gl.js");
 
@@ -11,10 +12,11 @@ import ReactMapGL, { Marker } from "react-map-gl";
 // });
 
 function App() {
+  const [poiData, setPoiData] = useState([]);
   const [viewport, setViewport] = useState({
     latitude: 47.6062,
     longitude: -122.3321,
-    zoom: 10,
+    zoom: 12,
     width: "90vw",
     height: "90vh",
   });
@@ -22,22 +24,32 @@ function App() {
   const getPoiList = () => {
     axios
       .get(
-        `https://api.openchargemap.io/v3/poi?key=${process.env.REACT_APP_OPENCHARGE}&distanceunit=5`
+        `https://api.openchargemap.io/v3/poi?key=${process.env.REACT_APP_OPENCHARGE}&distanceunit=15&maxresults=100&latitude=47.6062&longitude=-122.3321`
       )
+      // go through the api and grab the coordinates for each charging ports
       .then((response) => {
-        console.log(response.data);
+        const newData = response.data.map((poi) => {
+          // console.log(poi.AddressInfo);
+          return {
+            id: poi.AddressInfo.ID,
+            lat: poi.AddressInfo.Latitude,
+            long: poi.AddressInfo.Longitude,
+          };
+        });
+        setPoiData(newData);
       })
       .catch((error) => {
         console.log(error.response.data);
       });
   };
 
+  // console.log(poiData);
   useEffect(getPoiList, []);
 
   return (
     <div className="App">
       <header>eevee App</header>
-      {/* <ReactMapGL
+      <ReactMapGL
         {...viewport}
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
         mapStyle="mapbox://styles/mapbox/streets-v11"
@@ -46,7 +58,13 @@ function App() {
         }}
       >
         SHOW ME YO MAPPPPPP!
-      </ReactMapGL> */}
+        {poiData.map((poi) => (
+          <Marker key={poi.id} latitude={poi.lat} longitude={poi.long}>
+            {/* <img src="redpin" /> */}
+            <div>X</div>
+          </Marker>
+        ))}
+      </ReactMapGL>
     </div>
   );
 }
